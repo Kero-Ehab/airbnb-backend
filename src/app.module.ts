f17c697d034588698d71375bfdba1d4684633aad
Kerolos <kerolosehabgaber@gmail.com>
2026-07-18 00:47:15 +0300

    init users module and users schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoreModule } from './core.module';
+import { UsersModule } from './users/users.module';
 
 
 @Module({
   imports: [
-    CoreModule
+    CoreModule,
+    UsersModule
   ],
   controllers: [AppController],
   providers: [AppService],
