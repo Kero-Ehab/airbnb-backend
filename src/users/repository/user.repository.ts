6994681f91471e9107534f883dda20bd26ccfc4b
Kerolos <kerolosehabgaber@gmail.com>
2026-07-20 22:02:06 +0300

    create user repository extends base repository

---
+++ src/users/repository/user.repository.ts
@@ -0,0 +1,16 @@
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { BaseRepository, ModelNames } from "src/common/data-access";
+import { User } from "../schemas/user.schema";
+import { HydratedDocument, Model } from "mongoose";
+
+
+@Injectable()
+export class UerRepository extends BaseRepository<User>{
+    constructor(
+        @InjectModel(ModelNames.USERS)
+        private readonly userModel:Model<HydratedDocument<User>>
+    ){
+        super(userModel);
+    }
+}
