07f150175c329082a19613cf5dbf7d93d999dcc2
Kerolos <kerolosehabgaber@gmail.com>
2026-07-21 17:15:45 +0300

    finish update User Raw Use Case

--- src/users/use-cases/update-user-raw.usecase.ts
+++ src/users/use-cases/update-user-raw.usecase.ts
@@ -1,12 +1,23 @@
 import { Injectable } from "@nestjs/common";
-
-
-
-
+import { UserRepository } from "../repository/user.repository";
+import {ClientSession, UpdateQuery} from "mongoose";
+import { User } from "../schemas/user.schema";
 
 @Injectable()
 export class updateUserRawUseCase{
     constructor(
-        
+        private readonly userRepository:UserRepository
     ){}
+
+    async execute(
+        filterQuery:UpdateQuery<User>,
+        data:Record<string, unknown>,
+        session?:ClientSession
+    ):Promise<void>{
+        await this.userRepository.findOneAndUpdate(
+            filterQuery,
+            data, 
+            {session}
+        );
+    }
 }
