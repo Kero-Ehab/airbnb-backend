96a330f5b3d310028b9c8fabbd5ecdba3f7e2268
Kerolos <kerolosehabgaber@gmail.com>
2026-07-23 14:56:23 +0300

    finsih refresh token dto

---
+++ src/auth/dto/refresh-token.dto.ts
@@ -0,0 +1,13 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsString } from "class-validator";
+
+export class RefreshTokenDto{
+
+    @ApiProperty({
+        description:'JWT Refresh Token',
+        example:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'
+    })
+    @IsString()
+    @IsNotEmpty()
+    refreshToken:string;
+}
