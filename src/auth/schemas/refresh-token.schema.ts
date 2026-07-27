f4484d0f661f26d06015dfc87e7131b81f7d60a4
Kerolos <kerolosehabgaber@gmail.com>
2026-07-27 20:49:03 +0300

    add refresh token schema

--- src/auth/schemas/refresh-token.schema.ts
+++ src/auth/schemas/refresh-token.schema.ts
@@ -1,6 +1,12 @@
-import { Schema } from "@nestjs/mongoose";
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 
 @Schema({timestamps:true})
 export class RefreshToken{
+    @Prop({required: true})
+    userId:string
 
+    @Prop({required:true})
+    refreshToken:string    
 }
+
+export const RefreshTokenSchema = SchemaFactory.createForClass(RefreshToken)
