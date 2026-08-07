a26484b00c8961429beaaf1b2d64ff7c313f78c5
Kerolos <kerolosehabgaber@gmail.com>
2026-08-08 01:08:04 +0300

    add docker file , upload single file use case

---
+++ src/files-upload/use-cases/upload-single-file.usecase.ts
@@ -0,0 +1,21 @@
+import { Injectable } from "@nestjs/common";
+import { MulterFile } from "../types/multer-file.type";
+import { BadRequestExeption } from "src/common/errors-handling/custom-exceptions/bad-request.exception";
+
+
+@Injectable()
+export class uploadSingleFileUseCase{
+
+    private readonly baseUrl = process.env.APP_BASE_URL;
+
+    async execute(file:MulterFile):Promise<String>{
+        if(!file){
+            throw new BadRequestExeption("NO File Provided")
+        }
+        return this.buildPublicUrl(file.filename);
+    }
+
+    private buildPublicUrl(filename: string): string {
+        return `${this.baseUrl}/uploads/${filename}`;
+    }
+}
