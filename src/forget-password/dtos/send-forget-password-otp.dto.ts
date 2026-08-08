849eb0f9a3104e4400aa8cc94bfc6e1081ef5d3f
Kerolos <kerolosehabgaber@gmail.com>
2026-08-08 18:19:10 +0300

    add send forget password otp dto

---
+++ src/forget-password/dtos/send-forget-password-otp.dto.ts
@@ -0,0 +1,12 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsNotEmpty } from "class-validator";
+
+export class SendForgetPasswordOtpDto{
+    @ApiProperty({
+        description: 'Email address that will receive the forget-password OTP',
+        example: 'user@example.com',
+    })
+    @IsNotEmpty()
+    @IsEmail()
+    email: string;
+}
