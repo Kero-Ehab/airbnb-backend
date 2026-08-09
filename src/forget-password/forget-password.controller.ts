2fa7a87ee2305d3b62b0bf7bd6c928454701a9bf
Kerolos <kerolosehabgaber@gmail.com>
2026-08-09 21:20:51 +0300

    add forget password controller

---
+++ src/forget-password/forget-password.controller.ts
@@ -0,0 +1,26 @@
+import { Body, Controller, Post } from "@nestjs/common";
+import { ForgetPasswordService } from "./forget-password.service";
+import { SendForgetPasswordOtpDto } from "./dtos/send-forget-password-otp.dto";
+import { VerifyForgetPasswordOtpDto } from "./dtos/verify-forget-password-otp.dto";
+import { ResetPasswordDto } from "./dtos/reset-password.dto";
+
+
+@Controller('forget-password')
+export class ForgetPasswordController{
+    constructor(
+        private readonly forgetPasswordService: ForgetPasswordService
+    ) {}
+
+    @Post('/send')
+    async sendForgetPasswordOtp(@Body() body: SendForgetPasswordOtpDto): Promise<void> {
+        await this.forgetPasswordService.sendForgetPasswordOtp(body.email);
+    }
+    @Post('/verify')
+    async verifyForgetPasswordOtp(@Body() dto: VerifyForgetPasswordOtpDto): Promise<void> {
+        await this.forgetPasswordService.verifyForgetPasswordOtp(dto);
+    }
+    @Post('/reset')
+    async resetPassword(@Body() dto: ResetPasswordDto): Promise<void> {
+        await this.forgetPasswordService.resetPassword(dto);
+    }
+}
