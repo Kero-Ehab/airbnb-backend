fca97d886b30457f6cfb6c47cd41ea2a1c45ad47
Kerolos <kerolosehabgaber@gmail.com>
2026-08-15 01:44:11 +0300

    add create city use case

---
+++ src/cities/use-cases/create-city.usecase.ts
@@ -0,0 +1,32 @@
+import { BadRequestException, Injectable } from "@nestjs/common";
+import { CityRepository } from "../repository/city.repository";
+import { CountriesServices } from "src/countries/countries.service";
+import { CreateCityDto } from "../dtos/create-city.dto";
+import { plainToInstance } from "class-transformer";
+import { CityResponseDto } from "../dtos/city-response.dto";
+
+
+@Injectable()
+export class CreateCityUsecase{
+    constructor(
+        private readonly cityRepository:CityRepository,
+        private readonly countriesService:CountriesServices
+    ){}
+
+    async execute(body: CreateCityDto):Promise<CityResponseDto>{
+        
+        await this.countriesService.getCountryById(body.country)
+
+        const existingCityByName = await this.cityRepository.findOne({
+            name: body.name,
+            country: body.country,
+            isDeleted: false,
+        })
+
+        if(existingCityByName){
+            throw new BadRequestException('City already exists')
+        }
+        const city = await this.cityRepository.create(body)
+        return plainToInstance(CityResponseDto, city.toObject())
+    }
+}
