36e13fc382a0380f2c9472d4c07e3cf6d718c34f
Kerolos <kerolosehabgaber@gmail.com>
2026-08-15 00:50:04 +0300

    add create city dto add edit city response dto path

---
+++ src/cities/dtos/create-city.dto.ts
@@ -0,0 +1,15 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsMongoId, IsNotEmpty, IsString } from "class-validator";
+
+
+export class CreateCityDto{
+    @ApiProperty({ description: 'City name', example: 'Cairo' })
+    @IsString()
+    @IsNotEmpty()
+    name:string
+
+    @ApiProperty({ description: 'Country MongoDB ID', example: '60d21b4967d0d8992e610c85' })
+    @IsNotEmpty()
+    @IsMongoId()
+    country: string;
+}
