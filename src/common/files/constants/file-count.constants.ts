f9f620e1ea89aa8c2459d387b448c15d0612e966
Kerolos <kerolosehabgaber@gmail.com>
2026-09-07 21:04:49 +0300

    add file count constant and start unit controller

---
+++ src/common/files/constants/file-count.constants.ts
@@ -0,0 +1,3 @@
+export const MaxFileCount={
+    UNIT_IMAGES:5,
+}as const satisfies Record<string, number>;
---
+++ src/unit/units.controller.ts
@@ -0,0 +1,28 @@
+import { Controller, Post, UploadedFiles, UseInterceptors } from "@nestjs/common";
+import { UnitsService } from "./units.service";
+import { Authorize } from "src/auth/decorators/roles.decorator";
+import { Roles } from "src/common/constants/roles.constants";
+import { FilesInterceptor } from "@nestjs/platform-express";
+import { MaxFileCount } from "src/common/files/constants/file-count.constants";
+
+
+
+
+
+@Controller('units')
+export class UnitsController{
+    constructor(
+        private readonly unitsService: UnitsService
+    ){}
+
+    @Authorize(Roles.USER)
+    @Post()
+    @UseInterceptors(FilesInterceptor('photos', MaxFileCount.UNIT_IMAGES))
+    async create(
+        @UploadedFiles(createParseFilePipe())
+    ){}
+
+
+
+
+}
