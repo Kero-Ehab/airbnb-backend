439e19bd3d1f8a4d2177b4badf48f1606d5230b1
Kerolos <kerolosehabgaber@gmail.com>
2026-09-10 14:04:30 +0300

    add unit reviews  service

---
+++ src/unit-reviews/unit-reviews.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from "@nestjs/common";
+import { CreateUnitReviewUseCase } from "./usecases/create-unit-review.usecase";
+import { CalculateRatingAvgUseCase } from "./usecases/calculate-rating-avg.usecase";
+import { FindUnitReviewsUsecase } from "./usecases/find-unit-reviews.usecase";
+import { CreateUnitReviewDto } from "./dtos/create-unit-review.dto";
+import { ClientSession } from "mongoose";
+import { CalculateRatingAvgAndCountDto } from "./dtos/calculate-rating-avg-and-count.dto";
+import { FindUnitReviewDto } from "./dtos/find-unit-reviews.dto";
+import { PaginationResult } from "src/common/data-access";
+import { FindUnitReviewResponseDto } from "./dtos/find-unit-review-response.dto";
+
+@Injectable()
+export class UnitReviewsService{
+    constructor(
+        private readonly createUnitReviewUseCase: CreateUnitReviewUseCase,
+        private readonly calculateRatingAvgUseCase: CalculateRatingAvgUseCase,
+        private readonly findUnitReviewsUsecase: FindUnitReviewsUsecase,
+    ){}
+
+    async createUnitReview(
+        body: CreateUnitReviewDto,
+        session?: ClientSession
+    ):Promise<void>{
+        await this.createUnitReviewUseCase.execute(body, session);
+    }
+
+    async calculateRatingAvg(
+        unitId:string,
+        session?: ClientSession
+    ):Promise<CalculateRatingAvgAndCountDto>{
+        return this.calculateRatingAvgUseCase.execute(unitId, session)
+    }
+
+    async findUnitReviews(
+        query: FindUnitReviewDto,
+    ):Promise<PaginationResult<FindUnitReviewResponseDto>>{
+        return this.findUnitReviewsUsecase.execute(query)
+    }
+}
