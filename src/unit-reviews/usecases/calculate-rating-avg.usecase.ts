f538d073875358cda58efc6a27afca275cd84505
Kerolos <kerolosehabgaber@gmail.com>
2026-09-10 13:41:12 +0300

    add calculate rating avg use case

---
+++ src/unit-reviews/usecases/calculate-rating-avg.usecase.ts
@@ -0,0 +1,51 @@
+import { Injectable } from "@nestjs/common";
+import { UnitReviewsRepository } from "../repositories/unit-reviews.repository";
+import { ClientSession } from "mongoose";
+import { CalculateRatingAvgAndCountDto } from "../dtos/calculate-rating-avg-and-count.dto";
+
+@Injectable()
+export class CalculateRatingAvgUseCase{
+
+    constructor(
+        private readonly unitReviewsRepository: UnitReviewsRepository
+    ){}
+
+    async execute(
+        unitId: string,
+        session?:ClientSession
+    ):Promise<CalculateRatingAvgAndCountDto>{
+        const result = await this.unitReviewsRepository.aggregate<{
+            ratingAvg: number;
+            ratingCount: number;
+        }>(
+            [
+                {
+                    $match:{unit: unitId},
+                },
+                {
+                    $group:{
+                        _id: '$unit',
+                        ratingAvg: {$avg:'$rating'},
+                        ratingCount: {$sum:1}
+                    },
+                },
+                {
+                    $project:{
+                        _id:0,
+                        ratingAvg:1,
+                        ratingCount:1
+                    }
+                }
+            ],
+            {session}
+        );
+        console.log('result', result);
+
+        if(result.length === 0){
+            return {ratingAvg:0, ratingCount:0}
+        }
+        
+        const { ratingAvg, ratingCount } = result[0];
+        return { ratingAvg, ratingCount };
+    }
+}
