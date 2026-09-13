106c14a85f942b133767a2fdea88af0f391e8f18
Kerolos <kerolosehabgaber@gmail.com>
2026-09-13 14:00:34 +0300

    add booking schema

--- src/bookings/schema/booking.schema.ts
+++ src/bookings/schema/booking.schema.ts
@@ -1,66 +1,67 @@
-import { Prop, Schema } from "@nestjs/mongoose";
-import { ModelNames } from "src/common/data-access";
-import { BookingStatus } from "../enums/booking-status.enum";
-import { CancelBy } from "../enums/cancel-by.enum";
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ModelNames } from '../../common/data-access';
+import { BookingStatus } from '../enums/booking-status.enum';
+import { CancelBy } from '../enums/cancel-by.enum';
+import { GuestReview } from './subdocument/guest-review.schema';
 
-
-@Schema({timestamps: true})
+@Schema({ timestamps: true })
 export class Booking {
+  @Prop({ required: true, ref: ModelNames.UNITS })
+  unit: string;
 
-    @Prop({required: true, ref:ModelNames.UNITS})
-    unit: string;
+  @Prop({ required: true, ref: ModelNames.USERS })
+  guest: string;
 
-    @Prop({required: true, ref:ModelNames.USERS})
-    guest: string
+  @Prop({ required: true, ref: ModelNames.USERS })
+  host: string;
 
-    @Prop({required: true, ref:ModelNames.USERS})
-    host:string;
-    
-    @Prop({required: true, type: Date})
-    checkIn: number| Date;
+  @Prop({ required: true, type: Date })
+  checkIn: number | Date;
 
-    @Prop({required:true, type: Date})
-    checkOut: number| Date;
+  @Prop({ required: true, type: Date })
+  checkOut: number | Date;
 
-    @Prop({required:true})
-    dayCount: number;
+  @Prop({ required: true })
+  daysCount: number;
 
-    @Prop({required: true})
-    pricePerDay: number;
+  @Prop({ required: true })
+  pricePerDay: number;
 
-    @Prop({required: true})
-    bookingAmount: number;
+  @Prop({ required: true })
+  bookingAmount: number;
 
-    @Prop({required: true, default: 0})
-    vat: number
+  @Prop({ required: true, default: 0 })
+  vat: number;
 
-    @Prop({required: true, default: 0})
-    vatAmount: number
+  @Prop({ required: true, default: 0 })
+  vatAmount: number;
 
-    @Prop({required: true})
-    totalAmount:number
+  @Prop({ required: true })
+  totalAmount: number;
 
-    @Prop()
-    adultsCount?:number;
+  @Prop()
+  adultsCount?: number;
 
-    @Prop()
-    kidsCount?: number;
+  @Prop()
+  kidsCount?: number;
 
-    @Prop()
-    notes?: string;
+  @Prop()
+  notes?: string;
 
-    @Prop({type: String, enum:BookingStatus, default: BookingStatus.PENDING})
-    status: BookingStatus;
+  @Prop({ type: String, enum: BookingStatus, default: BookingStatus.PENDING })
+  status: BookingStatus;
 
-    @Prop()
-    cancellationReason?: string;
+  @Prop()
+  cancellationReason?: string;
 
-    @Prop()
-    cancellationDate?: Date;
+  @Prop()
+  cancellationDate?: Date;
 
-    @Prop({type: String, enum: CancelBy})
-    cancelBy?: CancelBy;
+  @Prop({ type: String, enum: CancelBy })
+  cancelBy?: CancelBy;
 
-    // @Prop()
-    // guestReview?: GuestReview;
+  @Prop()
+  guestReview?: GuestReview;
 }
+
+export const BookingSchema = SchemaFactory.createForClass(Booking);
